95d1396950e6f6d402dd53b4a58d225aa669478d
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-03 09:26:40 -0300

    setup da api

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
