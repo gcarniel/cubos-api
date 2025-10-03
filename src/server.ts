9ed968ea7dd765dd4586ff9aa1c1e08245fd7f15
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-03 12:01:35 -0300

    registrando error handler e rotas de auth

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.js";
+import { authRoutes} from "./routes/auth-route.js";
+import { errorHandler } from "./errors/error-handler.js";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
+app.setErrorHandler(errorHandler)
+
 app.register(fastifyCors);
 
-app.listen({ port: 3333 }).then(() => {
+app.register(authRoutes, {prefix: '/auth'})
+
+app.get('/health', async () => {
+  return { status: 'ok', timestamp: new Date().toISOString() }
+})
+
+app.listen({ port: env.PORT, host: env.HOST }).then(() => {
   console.log("HTTP server running!");
 });
