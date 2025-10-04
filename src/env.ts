8f2da069b2734a02c8f414bca7bab30accc86867
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-04 15:24:47 -0300

    adicao de variaveis de ambiente para bucket na aws

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,26 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+    .enum(['development', 'production', 'test'])
+    .default('development'),
   PORT: z.coerce.number().default(3333),
-  HOST: z.string().default("0.0.0.0"),
+  HOST: z.string().default('0.0.0.0'),
   DATABASE_URL: z.url(),
   JWT_SECRET: z.string(),
-});
+  AWS_S3_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_REGION: z.string(),
+})
 
-export type Env = z.infer<typeof envSchema>;
+export type Env = z.infer<typeof envSchema>
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  console.error("❌ Invalid environment variables:", _env.error.format());
-  throw new Error("Invalid environment variables");
+  console.error('❌ Invalid environment variables:', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
